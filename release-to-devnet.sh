9dab10617896c0ff68cd9fe4c07ac025ba9c14c5
microwavedcola1 <microwavedcola@gmail.com>
2022-10-19 15:22:32 +0200

    switch to released anchor
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- release-to-devnet.sh
+++ release-to-devnet.sh
@@ -4,9 +4,9 @@ set -ex pipefail
 
 WALLET_WITH_FUNDS=~/.config/solana/mango-devnet.json
 PROGRAM_ID=m3roABq4Ta3sGyFRLdY4LH1KN16zBtg586gJ3UxoBzb
-cargo run --manifest-path ../mango-v4/anchor/cli/Cargo.toml -- build
+anchor build
 ./idl-fixup.sh
 cp -v ./target/types/mango_v3_reimbursement.ts ./ts/client/src/mango_v3_reimbursement.ts
 yarn tsc
 solana --url https://mango.devnet.rpcpool.com program deploy --program-id $PROGRAM_ID -k $WALLET_WITH_FUNDS target/deploy/mango_v3_reimbursement.so --skip-fee-check
-cargo run --manifest-path ../mango-v4/anchor/cli/Cargo.toml -- idl upgrade --provider.cluster https://mango.devnet.rpcpool.com --provider.wallet $WALLET_WITH_FUNDS --filepath target/idl/mango_v3_reimbursement.json $PROGRAM_ID
+anchor idl upgrade --provider.cluster https://mango.devnet.rpcpool.com --provider.wallet $WALLET_WITH_FUNDS --filepath target/idl/mango_v3_reimbursement.json $PROGRAM_ID
